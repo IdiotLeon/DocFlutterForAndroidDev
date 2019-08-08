654dc4d37e3c23e05f4de2c5c1b76c9817c3b0d4
Leon <yanglyu.leon.7@gmail.com>
2019-08-08 03:19:29 -0700

    tutorial: Painting on a canvas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(FadeAppTest());
+  // runApp(SampleApp());
+  // runApp(FadeAppTest());
+  runApp(MaterialApp(home: PaintApp()));
 }
 
 class SampleApp extends StatelessWidget {
@@ -122,4 +124,51 @@ class _MyFadeTest extends State<MyFadeTest> with TickerProviderStateMixin {
       ),
     );
   }
+}
+
+class PaintApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Signature());
+}
+
+class Signature extends StatefulWidget {
+  @override
+  SignatureState createState() => SignatureState();
+}
+
+class SignatureState extends State<Signature> {
+  List<Offset> _points = <Offset>[];
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: (DragUpdateDetails details) {
+        setState(() {
+          RenderBox referenceBox = context.findRenderObject();
+          Offset localPosition =
+              referenceBox.globalToLocal(details.globalPosition);
+          _points = List.from(_points)..add(localPosition);
+        });
+      },
+      onPanEnd: (DragEndDetails details) => _points.add(null),
+      child:
+          CustomPaint(painter: SignaturePainter(_points), size: Size.infinite),
+    );
+  }
+}
+
+class SignaturePainter extends CustomPainter {
+  SignaturePainter(this.points);
+  final List<Offset> points;
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null)
+        canvas.drawLine(points[i], points[i + 1], paint);
+    }
+  }
+
+  bool shouldRepaint(SignaturePainter other) => other.points != points;
 }
